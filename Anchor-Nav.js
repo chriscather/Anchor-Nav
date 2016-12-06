76b176457dbdf0bb1ba26c85f39c08a3f4c3f309
Chris Cather <auciker@yahoo.com>
2016-12-05 16:16:05 -0800

    Update Anchor-Nav.js

--- Anchor-Nav.js
+++ Anchor-Nav.js
@@ -1,8 +1,9 @@
+// SMOOTH SCROLL
 $(function() {
 	$('a[href*="#"]:not([href="#"])').click(function() {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 			if (target.length) {
 				$('html, body').animate({
 					scrollTop: target.offset().top
@@ -13,19 +14,21 @@ $(function() {
 	});
 });
 
+// ANCHOR NAV
 $(window).scroll(function() {
 	var windscroll = $(window).scrollTop();
 	var nextSection = $('.active').next('li').children('a').attr('href');
+
 	if (document.body.scrollHeight - $(this).scrollTop() <= $(this).height()) {
 		nextSection = $('#menu li:first-child a').attr('href');
 		$('.back-top').addClass('bottom');
-		} else {
+	} else {
 		$('.back-top').removeClass('bottom');
 	}
-	
+
 	if (windscroll >= 100) {
 		$('section').each(function(i) {
-			if ($(this).position().top <= windscroll + 100) {
+			if ($(this).position().top <= windscroll+100) {
 				$('#menu li.active').removeClass('active');
 				$('#menu li').eq(i).addClass('active');
 				$('.back-top').attr('href', nextSection);
